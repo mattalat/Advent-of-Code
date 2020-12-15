a410d4cab10cb1ed31bc58d757bf43fb343694bf
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2020-12-15 17:59:55 -0500

    Use readlines and add quicker alias

--- 2020/advent.rb
+++ 2020/advent.rb
@@ -3,8 +3,12 @@
 module Advent
   module_function
 
-  def load_input_into_array(input_filename = 'input.txt')
-    called_from = File.dirname(caller_locations.first.path)
-    File.read(File.join(called_from, input_filename)).split.map(&:strip)
+  def load_input_into_array(filename: 'input.txt', caller_loc: caller_locations)
+    called_from = File.dirname(caller_loc.first.path)
+    File.open(File.join(called_from, filename)).readlines
+  end
+
+  def read(caller_loc: caller_locations)
+    load_input_into_array caller_loc: caller_loc
   end
 end
