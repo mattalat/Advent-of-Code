1565655c8b251b1b3f21cb1937f89aa88f327fab
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-01 18:43:45 -0500

    Compress part2 even more

--- 2021/day01/part2.rb
+++ 2021/day01/part2.rb
@@ -3,5 +3,4 @@
 input = File.open(File.expand_path('input', __dir__)).readlines
 
 depths = input.map(&:chomp).map(&:to_i)
-slices = depths.each_cons(3).to_a
-p slices[..-2].zip(slices[1..]).map { _1.sum - _2.sum }.count(&:negative?)
+p depths.each_cons(3).each_cons(2).map { _1.sum - _2.sum }.count(&:negative?)
