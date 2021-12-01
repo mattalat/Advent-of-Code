7f0a0b535d92b6d79d1cd022fd82b9f284934419
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-01 18:37:53 -0500

    Compress part2

--- 2021/day01/part2.rb
+++ 2021/day01/part2.rb
@@ -3,12 +3,5 @@
 input = File.open(File.expand_path('input', __dir__)).readlines
 
 depths = input.map(&:chomp).map(&:to_i)
-increases = 0
-
-depths[3..].inject(depths[..2]) do |window, this|
-  new_window = [window[-2], window[-1], this].compact
-  increases += 1 if new_window.sum > window.sum
-  new_window
-end
-
-p increases
+slices = depths.each_cons(3).to_a
+p slices[..-2].zip(slices[1..]).map { _1.sum - _2.sum }.count(&:negative?)
