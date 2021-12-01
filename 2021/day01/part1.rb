942fb9a0dd7034645054ae007fab7650e62d4a08
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-01 18:19:34 -0500

    Compress part1

--- 2021/day01/part1.rb
+++ 2021/day01/part1.rb
@@ -3,11 +3,5 @@
 input = File.open(File.expand_path('input', __dir__)).readlines
 
 depths = input.map(&:chomp).map(&:to_i)
-increases = 0
 
-depths.inject(depths.first) do |last, this|
-  increases += 1 if this > last
-  this
-end
-
-p increases
+p depths.zip(depths[1..]).map { _1.compact.reduce(&:-) }.count(&:negative?)
