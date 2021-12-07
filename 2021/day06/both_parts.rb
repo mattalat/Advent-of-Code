3d59941c8df57407b445b2fa74e7a32004b1b3ec
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-06 21:01:54 -0500

    Compress day 6 solutions

--- 2021/day06/both_parts.rb
+++ 2021/day06/both_parts.rb
@@ -6,19 +6,11 @@ school = input.split(',').map(&:to_i).tally
 school.default = 0
 
 simulate_a_day = proc {
-  school.transform_keys! { _1 - 1 }
-
-  next unless school.key?(-1)
-
-  new_fish = school.delete(-1)
+  school.transform_keys!(&:pred)
+  new_fish = school.delete(-1) || 0
   school[6] += new_fish
   school[8] += new_fish
 }
 
-80.times(&simulate_a_day)
-
-puts school.values.sum
-
-(256 - 80).times(&simulate_a_day)
-
-puts school.values.sum
+80.times(&simulate_a_day).then { puts school.values.sum }
+(256 - 80).times(&simulate_a_day).then { puts school.values.sum }
