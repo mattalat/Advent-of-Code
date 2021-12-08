34951269eaf6c15570ac65f07fea19bfadaa1e98
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-08 17:01:07 -0500

    Simplify by using ActiveSupport

--- 2021/day08/both_parts.rb
+++ 2021/day08/both_parts.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'set'
+require 'active_support/core_ext/array'
 
 input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
 
@@ -9,8 +10,8 @@ displays = input.map { |l| l.split('|').map { _1.split(' ') } }
 p displays.map(&:last).flat_map { _1.map(&:length) }.select { [2, 3, 4, 7].include? _1 }.count
 
 numbers = displays.map do |wirings, outputs|
-  wirings.map! { |w| Set.new(w.split('')) }
-  outputs.map! { |w| Set.new(w.split('')) }
+  wirings.map! { Set.new(_1.split('')) }
+  outputs.map! { Set.new(_1.split('')) }
 
   mappings = {
     1 => wirings.find { _1.length == 2 },
@@ -23,19 +24,15 @@ numbers = displays.map do |wirings, outputs|
   fives = wirings.select { _1.length == 5 }
   sixes = wirings.select { _1.length == 6 }
 
-  mappings[9] = sixes.find { mappings[4].subset? _1 }
-  sixes -= [mappings[9]]
-  mappings[0] = sixes.find { mappings[7].subset? _1 }
-  sixes -= [mappings[0]]
+  mappings[9] = sixes.extract! { mappings[4].subset? _1 }.first
+  mappings[0] = sixes.extract! { mappings[7].subset? _1 }.first
   mappings[6] = sixes.first
 
-  mappings[5] = fives.find { mappings[6].superset? _1 }
-  fives -= [mappings[5]]
-  mappings[3] = fives.find { mappings[9].superset? _1 }
-  fives -= [mappings[3]]
+  mappings[5] = fives.extract! { mappings[6].superset? _1 }.first
+  mappings[3] = fives.extract! { mappings[9].superset? _1 }.first
   mappings[2] = fives.first
 
   outputs.map { mappings.invert[_1] }.join.to_i
 end
 
-p numbers.reduce(:+)
+p numbers.sum
