12c7b6fd3f396d608b60120bdd4252c33769a81f
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-09 18:43:35 -0500

    Simplify and clarify

--- 2021/day09/both_parts.rb
+++ 2021/day09/both_parts.rb
@@ -2,16 +2,16 @@
 
 input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
 
-pts = input.map { _1.split('').map(&:to_i) }
+pts = input.map { _1.chars.map(&:to_i) }
 pts.unshift(Array.new(pts[0].length, 9))
 pts.push(Array.new(pts[0].length, 9))
 pts.map! { [9, *_1, 9] }
-mins = []
 
 def neighbors(x1, y1)
   [[x1 - 1, y1], [x1 + 1, y1], [x1, y1 - 1], [x1, y1 + 1]]
 end
 
+mins = []
 (1...pts.length - 1).each do |i|
   (1...pts[0].length - 1).each do |j|
     cell = pts[i][j]
@@ -19,24 +19,25 @@ end
   end
 end
 
-p mins.map { _1.pop + 1 }.sum
+p mins.map(&:pop).sum { _1 + 1 }
+
+# Part 2
 
 class Basin
-  attr_reader :pts, :x, :y, :visited
+  attr_reader :x, :y, :visited
 
-  def initialize(pts, x, y)
-    @pts = pts
+  def initialize(x, y)
     @x = x
     @y = y
     @visited = []
   end
 
   def size(x1 = x, y1 = y)
-    current = pts[x1][y1]
+    current = PTS[x1][y1]
     visited << [x1, y1]
 
     neighbors(x1, y1).sum do |x2, y2|
-      neighbor = pts[x2][y2]
+      neighbor = PTS[x2][y2]
       next 0 if neighbor == 9 || current > neighbor || visited.include?([x2, y2])
 
       size(x2, y2)
@@ -44,6 +45,7 @@ class Basin
   end
 end
 
-basins = mins.map { |i, j| Basin.new(pts, i, j).size }
+PTS = pts.clone.freeze
+sizes = mins.map { |i, j| Basin.new(i, j).size }
 
-p basins.sort[-3..].reduce(:*)
+p sizes.sort[-3..].reduce(:*)
