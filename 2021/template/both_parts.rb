eda46298abd3dbc28e79dc3ff9d3d82817abb73b
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-09 19:51:32 -0500

    Change template to reflect past few days

--- 2021/template/both_parts.rb
+++ 2021/template/both_parts.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
+require 'byebug'
 
 input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
+
+byebug
+puts 'done'
--- 2021/template/part2.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
