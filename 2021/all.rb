bba64bb5efc966bbefee464f8b3b0978f7b376a8
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-09 19:35:23 -0500

    Play around with tty for pretty output

--- 2021/all.rb
+++ 2021/all.rb
@@ -2,23 +2,24 @@
 
 require 'benchmark'
 require 'terminal-table'
+require 'tty-command'
+require 'tty-logger'
+require 'tty-progressbar'
 
-puts 'Running...'
+log = TTY::Logger.new
+bar = TTY::ProgressBar.new('Running [:bar]', total: Dir['day*'].count)
+cmd = TTY::Command.new(printer: :null)
 
 rows = Dir['day*'].sort.map do |day|
+  bar.advance
   files = Dir["#{day}/*.rb"].grep(/(part\d|both_parts)\.rb/)
-
-  print "\r#{day}"
-  running_time = Benchmark.realtime { files.each { |file| `ruby #{file}` } }
-
+  running_time = Benchmark.realtime { files.each { cmd.run('ruby', _1) } }
   [day.scan(/\d+/).first.gsub(/^0/, ''), format('%<time>.3f', time: running_time).gsub(/^0+/, '')]
 end
-
-print "\33[2K\rDone\n\n"
 
 table = Terminal::Table.new headings: ['Day', 'Time (s)'], rows: rows
 table.align_column(0, :right)
 table.align_column(1, :right)
+puts "\n#{table}\n\n"
 
-puts table
-puts "\nTotal running time: #{format('%<sum>0.2f', sum: rows.map(&:last).map(&:to_f).sum)} s"
+log.success 'Total', format('%<sum>0.2f', sum: rows.map(&:last).map(&:to_f).sum), 's'
