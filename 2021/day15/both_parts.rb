0c492b219280ba7b901e73766d8d037957800278
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-15 18:31:34 -0500

    Finish day 15

---
+++ 2021/day15/both_parts.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext/object'
+require 'active_support/core_ext/array'
+
+input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
+
+class Solver
+  attr_reader :coords, :w, :h, :source, :destination
+  attr_accessor :graph, :pq
+
+  def initialize(coords)
+    @w = coords.first.length
+    @h = coords.length
+    @coords = coords.flatten
+
+    # Graph nodes are stored as follows:
+    # 0       1                     2
+    # index   distance from source  previous node
+    @graph = (0...@coords.length).map { |i| [i, Float::INFINITY, nil] }
+
+    @graph[0][1] = 0
+    @source = 0
+    @destination = @coords.length - 1
+    @pq = [graph[0]]
+  end
+
+  def solve
+    dijkstra
+    calculate_risk
+  end
+
+  private
+
+  def adjacent_to(xy)
+    [
+      (xy - w if xy >= w),
+      (xy + w if xy < (w * h) - w),
+      (xy + 1 unless xy % w == w - 1),
+      (xy - 1 unless (xy % w).zero?)
+    ].compact
+  end
+
+  def dijkstra
+    until pq.empty?
+      u = pq.min_by { _1[1] }
+      self.pq -= [u]
+
+      adjacent_to(u[0]).map { graph[_1] }.each do |v|
+        alt = u[1] + coords[v[0]]
+        next unless v[1] > alt
+
+        v[1] = alt
+        v[2] = u[0]
+        pq.push(v)
+      end
+    end
+  end
+
+  def calculate_risk
+    u = destination
+    stack = []
+
+    while u.present?
+      stack.push(u)
+      u = graph[u][2]
+    end
+
+    stack.map { coords[_1] }.sum - coords[source]
+  end
+end
+
+# Part 1 input
+coords = input.map(&:chars).map { _1.map(&:to_i) }
+
+# Part 2 input
+tick_up = proc { |n, amt|
+  add = n + amt
+  add > 9 ? add % 9 : add
+}
+new_col    = (0...5).map { |n| coords.map { |r| r.map { |i| tick_up.call(i, n) } } }.flatten(1)
+new_cols   = (0...5).map { |n| new_col.map { |r| r.map { |i| tick_up.call(i, n) } } }
+new_coords = new_cols.map(&:transpose).reduce(:+).transpose
+
+p Solver.new(coords).solve
+p Solver.new(new_coords).solve
