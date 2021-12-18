a26005adbfa35541f23770df0ffefc020e4ebe06
Matt Lawrence <matthew.lawrence@pennmedicine.upenn.edu>
2021-12-17 22:01:25 -0500

    Improve ⚡️ with some hash tricks

--- 2021/day12/both_parts.rb
+++ 2021/day12/both_parts.rb
@@ -3,10 +3,20 @@
 input = File.open(File.expand_path('input', __dir__)).readlines.map(&:chomp)
 
 class Graph
-  attr_accessor :edges
+  attr_accessor :edges, :new_edges, :trip
 
-  def initialize
+  def initialize(connection_strings)
     @edges = Hash.new { |h, k| h[k] = [].clone }
+    connections = connection_strings.map { _1.split('-') }
+    connections.each { |node, other| add_edge(node, other) }
+
+    # Starting cave cannot be reentered
+    @edges.each_key { |node| @edges[node] -= ['start'] }
+
+    transform_caves!
+
+    # Trip ends when ending cave is reached
+    edges.delete(trip[1])
   end
 
   def add_edge(node, other)
@@ -14,56 +24,76 @@ class Graph
     edges[other] << node
   end
 
-  def double_paths_between(start = 'start', last = 'end')
-    paths = [*path_between(start, last)]
-    small_caves = edges.keys.select { _1.downcase == _1 } - %w[start end]
-
-    small_caves.each do |other|
-      @double_node = other
-      paths.push(*paths_between(start, last))
-    end
-
-    paths.uniq
+  def visitable_small_caves
+    edges.keys.select { _1 < 10 } - trip
   end
 
-  def paths_between(start = 'start', last = 'end')
-    @all_paths = []
-    @path = []
-    @visited = []
-    @double_node ||= nil
-    path_between(start, last)
-    @all_paths
+  # Assuming there are fewer than 10 small caves, use integers [0..9] to signify
+  # small caves and [10..] to signify large caves. This drastically improves
+  # hash access.
+  def transform_caves!
+    sm = 0
+    bg = 10
+
+    transform_map = edges.keys.map { [_1] * 2 }.to_h.transform_values do |old_cave|
+      old_cave.downcase == old_cave ? sm += 1 : bg += 1
+    end
+
+    self.trip = [transform_map['start'], transform_map['end']]
+    self.edges = edges.map { |k, v| [transform_map[k], v.map { transform_map[_1] }] }.to_h
+  end
+end
+
+class Solver
+  attr_reader :graph
+
+  def initialize(graph)
+    @graph = graph
+  end
+
+  def single_paths
+    @double_node = nil
+    complete_paths
+  end
+
+  def single_and_double_paths
+    graph.visitable_small_caves.flat_map do |other|
+      @double_node = other
+      complete_paths
+    end.uniq
   end
 
   private
 
-  def path_between(start, last)
-    @visited << start if start.downcase == start
+  def complete_paths
+    @all_paths = []
+    @path = []
+    @visited = []
+    paths_to_end(graph.trip[0])
+    @all_paths
+  end
+
+  def visited?(node)
+    @visited.select { _1 == node }.count == (@double_node == node ? 2 : 1)
+  end
+
+  def paths_to_end(start)
+    @visited << start if start < 10
     @path << start
 
-    if start == last
+    if start == graph.trip[1]
       @all_paths << @path.dup
     else
-      edges[start].each do |other|
-        next if @visited.grep(/^#{other}$/).count == (@double_node == other ? 2 : 1)
-
-        path_between(other, last)
-      end
+      graph.edges[start].each { |other| paths_to_end(other) unless visited?(other) }
     end
 
     @path.pop
-    i = @visited.index(start)
-    @visited.slice!(i) unless i.nil?
+    @visited.slice!(@visited.index(start)) if @visited.include?(start)
   end
 end
 
-connections = input.map { _1.split('-') }
+graph = Graph.new(input)
+solver = Solver.new(graph)
 
-graph = Graph.new
-
-connections.each do |node, other|
-  graph.add_edge(node, other)
-end
-
-p graph.paths_between.length
-p graph.double_paths_between.length
+p solver.single_paths.length
+p solver.single_and_double_paths.length
